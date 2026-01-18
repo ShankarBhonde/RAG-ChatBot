2c78466a9b54592513d790adb68ee7cf753ae347
Shankar Bhonde <96866856+ShankarBhonde@users.noreply.github.com>
2026-01-19 00:31:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from PyPDF2 import PdfReader
+#from PyPDF2 import PdfReader
+from pypdf import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
